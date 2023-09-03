0282712d3e81f28247c592f895143a37ec780b3d
jacobryanheath <jake.heath91@gmail.com>
2023-09-03 08:29:12 -0700

    dns not working

--- common.sh
+++ common.sh
@@ -1,13 +1,7 @@
-# temporarily update resolv.conf in case that mirrors could not be resolved
-cat <<EOF >> /etc/resolv.conf
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-EOF
-systemctl restart systemd-networkd
-ufw allow 51871/udp
-ufw allow in from ${INTERNAL_IP_BASE}/16
-
 apt -y update && apt -y upgrade && apt install -y net-tools resolvconf wireguard
 cp /vagrant/${WG_CONFIG_FILE} /etc/wireguard/wg0.conf
 systemctl enable wg-quick@wg0
 systemctl start wg-quick@wg0
+
+ufw allow 51871/udp
+ufw allow in from ${INTERNAL_IP_BASE}/16
