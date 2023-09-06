6c899733a20aecbeaae7d1b16d170e00fc60ec1c
jacobryanheath <jake.heath91@gmail.com>
2023-09-05 22:40:39 -0700

    argocd script updates

--- tools/argocd.sh
+++ tools/argocd.sh
@@ -1,12 +1,9 @@
 #/bin/bash
 set -e
-
-kubectl port-forward service/argocd-server -n argocd 9999:https &> /dev/null &
-
 SERVER=localhost:9999
 ARGOCD=argocd
+ARGOCD_NAMESPACE=argocd
 USERNAME=admin
-PASSWORD=$(kubectl get secret argocd-initial-admin-secret -n argocd  -o json | jq '  {name: .metadata.name,data: .data|map_values(@base64d)}' | jq -r '.data.password')
 APP_NAME=homelab
 REPO_NAME=https://github.com/heathj/k3s-vagrant.git
 # must be a directory in repo
@@ -14,6 +11,9 @@ REPO_PATH=infra
 NAMESPACE=homelab
 
 # install ArgoCD
+kubectl create namespace ${ARGOCD_NAMESPACE}
+# todo: switch to the core packages 
+kubectl apply -n ${ARGOCD_NAMESPACE} -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 if ! command -v ${ARGOCD} &> /dev/null
 then
     curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
@@ -21,7 +21,15 @@ then
     rm argocd-linux-amd64
 fi
 
+# the secret doesn't show up right away
+sleep 180
+until PASSWORD=$(kubectl get secret argocd-initial-admin-secret -n ${ARGOCD_NAMESPACE}  -o json | jq '  {name: .metadata.name,data: .data|map_values(@base64d)}' | jq -r '.data.password')
+do
+    echo "waiting for secret..."
+    sleep 20
+done
+kubectl port-forward service/argocd-server -n ${ARGOCD_NAMESPACE} 9999:https &> /dev/null &
 ${ARGOCD} login ${SERVER} --insecure --username ${USERNAME} --password ${PASSWORD}
 kubectl create namespace ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
 ${ARGOCD} app create ${APP_NAME} --repo ${REPO_NAME} --path ${REPO_PATH} --dest-server https://kubernetes.default.svc --dest-namespace ${NAMESPACE} --server ${SERVER} --insecure
-argocd app sync ${APP_NAME}
+${ARGOCD} app sync ${APP_NAME}
