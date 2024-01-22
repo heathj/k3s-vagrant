76dc5a87e0522c4cb12297a6f64d8b1a9260043f
Jake Heath <jheath@chanzuckerberg.com>
2024-01-22 09:21:53 -0800

    go

--- infra/plex/helm_upgrade.sh
+++ infra/plex/helm_upgrade.sh
@@ -2,5 +2,6 @@
 set -euox pipefail
 
 kubectl create namespace plex --dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -f ./ingress.yml
 helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages  
-helm upgrade --install plex plex/plex-media-server --values values.yml -n plex
+helm upgrade --install plex plex/plex-media-server --version "0.1.8" --values values.yml -n plex
---
+++ infra/traefik/helm_upgrade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euox pipefail
+
+helm repo add traefik https://traefik.github.io/charts
+helm upgrade --install traefik traefik/traefik --version "26.0.0" --values values.yml -n kube-system
