f062cdcd5af61d3662e708abd775e0c5a251d3cd
Jake Heath <jheath@chanzuckerberg.com>
2024-02-17 07:44:45 -0800

    kubeconfig

---
+++ tools/kubeconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euox pipefail
+
+export HOMELAB_KUBECONFIG="${HOME}/.kube/home-lab-config"
+ssh jheath@node1.local "sudo cat /etc/rancher/k3s/k3s.yaml" > ${HOMELAB_KUBECONFIG}
+sed -i '' 's/127\.0\.0\.1/10\.0\.4\.200/g' ${HOMELAB_KUBECONFIG}
+sed -i '' 's/default/home-lab/g' ${HOMELAB_KUBECONFIG}
+
+export KUBECONFIG="${HOMELAB_KUBECONFIG};${HOME}/.kube/config"
