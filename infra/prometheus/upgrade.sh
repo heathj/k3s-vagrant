9c6eaeb9b5e16a40569a8a697446158f89fe1782
Jake Heath <jheath@chanzuckerberg.com>
2024-10-07 07:47:07 -0700

    add plex values back

--- infra/prometheus/upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-
-kubectx home-lab
-kubectl create namespace metrics --dry-run=client -o yaml | kubectl apply -f -
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts 
-helm repo update
-helm upgrade --install prometheus-stack prometheus-community/kube-prometheus-stack -n metrics --version 56.2.0
