c96475f873057fde2dbf4998b5adebe05c8006fa
Jake Heath <jheath@chanzuckerberg.com>
2024-10-07 08:50:16 -0700

    remove old infra folders

--- infra/plex/upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-
-kubectx home-lab
-kubectl create namespace plex --dry-run=client -o yaml | kubectl apply -f -
-kubectl apply -f ./ingress.yml
-helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages  
-helm upgrade --install plex plex/plex-media-server --version "0.1.8" --values values.yml -n plex
