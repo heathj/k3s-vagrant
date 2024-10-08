9702db0fc7675a959052290016b65abcec82a8e7
Jake Heath <jheath@chanzuckerberg.com>
2024-10-07 22:18:54 -0700

    remove unused folder

--- infra/traefik/upgrade.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-
-kubectx home-lab
-helm repo add traefik https://traefik.github.io/charts
-helm upgrade --install traefik traefik/traefik --version "26.0.0" --values values.yml -n kube-system
