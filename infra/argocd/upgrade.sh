7422e33d358b4cef1e68e7e05b541e9468de5902
Jake Heath <jheath@chanzuckerberg.com>
2024-11-18 21:09:43 -0800

    feat: install istio // remove traefik

--- infra/argocd/upgrade.sh
+++ infra/argocd/upgrade.sh
@@ -7,4 +7,4 @@ kubectl create namespace argo --dry-run=client -o yaml | kubectl apply -f -
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
-helm upgrade --install argo argo/argo-cd -n argo --version "7.6.8" --values values.yml
+helm upgrade --install argo argo/argo-cd -n argo --version "7.7.1" --values values.yml
--- tools/nettools.sh
+++
@@ -1,4 +0,0 @@
-NS=kube-system
-POD_NAME=nettools
-kubectl run -it --image=ubuntu ${POD_NAME} --restart=Never --namespace=${NS}
-kubectl delete pod ${POD_NAME} -n ${NS}
